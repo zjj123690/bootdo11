56410332bc9ff7d62abdd4883441d3a98bbc9be4
浪个里个浪 <573897054@qq.com>
2019-05-21 16:21:43 +0800

    ssss

--- src/main/java/com/bootdo/activiti/config/ActivitiConfig.java
+++ src/main/java/com/bootdo/activiti/config/ActivitiConfig.java
@@ -12,6 +12,7 @@ import javax.sql.DataSource;
 @Configuration
 public class ActivitiConfig {
     //流程配置，与spring整合采用SpringProcessEngineConfiguration这个实现
+    //dhsbi
     @Bean
     public ProcessEngineConfiguration processEngineConfiguration(DataSource dataSource, PlatformTransactionManager transactionManager){
         SpringProcessEngineConfiguration processEngineConfiguration = new SpringProcessEngineConfiguration();
